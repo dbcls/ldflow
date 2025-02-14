2902821ffce8d6d98f46f1a55c70b2806bcd0638
Daisuke Satoh <dsatoh@kamonohashi.co.jp>
2025-02-14 12:55:10 +0900

    initial commit

---
+++ lib/rdfconfig/jsonld.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "jsonld/version"
+
+module Rdfconfig
+  module Jsonld
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/rdfconfig/jsonld_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rdfconfig::Jsonld do
+  it "has a version number" do
+    expect(Rdfconfig::Jsonld::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
