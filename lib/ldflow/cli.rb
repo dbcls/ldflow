1c53bbd18516fb030e6ea3a01aace7b38628587d
Daisuke Satoh <dsatoh@kamonohashi.co.jp>
2025-03-31 15:28:52 +0900

    add setup command

--- lib/ldflow/cli.rb
+++ lib/ldflow/cli.rb
@@ -2,6 +2,7 @@
 
 require 'ldflow'
 
+require 'dotenv'
 require 'thor'
 
 module Ldflow
@@ -15,6 +16,60 @@ module Ldflow
         def exit_on_failure?
           true
         end
+
+        def source_root
+          File.expand_path('lib/ldflow/templates', Ldflow::GEM_ROOT)
+        end
+      end
+
+      desc 'setup', 'Setup ldflow'
+
+      option :force, aliases: '-f', type: :boolean, desc: 'Overwrite files that already exist'
+      option :pretend, aliases: '-p', type: :boolean, desc: 'Run but do not make any changes'
+
+      option :elasticsearch, type: :string, default: '8.17.4', desc: 'Elasticsearch version'
+      option :superset, type: :string, default: '4.1.2', desc: 'Superset version'
+
+      option :kibana, type: :boolean, default: false, desc: 'Enable kibana'
+      option :nodes, type: :numeric, default: 1, desc: 'Number of elasticsearch nodes'
+      option :superset_examples, type: :boolean, desc: 'Load examples to superset'
+
+      def setup
+        puts Middleware.version
+
+        Middleware.check unless options[:pretend]
+
+        if (env_file = Ldflow.home.join('.env')).exist?
+          env = Dotenv.parse(env_file)
+          @elasticsearch_password = env['ES_PASSWORD'] if env['ES_PASSWORD'] && !env['ES_PASSWORD'].empty?
+          @kibana_password = env['KIBANA_PASSWORD'] if env['KIBANA_PASSWORD'] && !env['KIBANA_PASSWORD'].empty?
+          @superset_password = env['SS_PASSWORD'] if env['SS_PASSWORD'] && !env['SS_PASSWORD'].empty?
+        end
+
+        @elasticsearch_password ||= SecureRandom.alphanumeric
+        @kibana_password ||= SecureRandom.alphanumeric
+        @superset_password ||= SecureRandom.alphanumeric
+
+        @options = options
+
+        empty_directory Ldflow.home
+
+        template 'env.erb', Ldflow.home.join('.env'), context: binding
+        template 'docker-compose.yml.erb', Ldflow.home.join('docker-compose.yml'), context: binding
+
+        directory File.expand_path('vendor/superset/docker', Ldflow::GEM_ROOT), Ldflow.home.join('docker'), mode: :preserve
+        copy_file 'requirements-local.txt', Ldflow.home.join('docker', 'requirements-local.txt')
+        template 'superset.env.erb', Ldflow.home.join('docker', '.env-local'), context: binding
+
+        Middleware.setup unless options[:pretend]
+      end
+
+      desc 'compose', 'Subcommands for docker compose'
+
+      def compose(*args)
+        cmd = Middleware.compose + ['-f', Ldflow.home.join('docker-compose.yml').to_s] + args
+
+        run cmd.join(' ')
       end
 
       desc 'convert', 'Subcommands for file format conversion'
---
+++ lib/ldflow/middleware.rb
@@ -0,0 +1,93 @@
+# frozen_string_literal: true
+
+require 'dotenv'
+
+module Ldflow
+  class Middleware
+    module Container
+      ELASTICSEARCH = 'ldflow-elasticsearch'
+      KIBANA = 'ldflow-kibana'
+      SUPERSET_REDIS = 'ldflow-superset-redis'
+      SUPERSET_DB = 'ldflow-superset-db'
+      SUPERSET = 'ldflow-superset'
+      SUPERSET_WORKER = 'ldflow-superset-worker'
+      SUPERSET_WORKER_BEAT = 'ldflow-superset-worker-beat'
+    end
+
+    class << self
+      def compose
+        @compose ||= command(%w[docker compose >/dev/null 2>&1]) do |_, status|
+          if status.success?
+            break %w[docker compose]
+          elsif available?('docker-compose')
+            break %w[docker-compose]
+          else
+            raise 'Docker compose is not installed'
+          end
+        end
+      end
+
+      def version
+        command(compose + ['version']).chomp
+      end
+
+      def check
+        check_container_running
+      end
+
+      def setup
+        init_superset
+      end
+
+      private
+
+      def command(*cmds)
+        out = `#{cmds.join(' ')}`
+        status = $CHILD_STATUS
+
+        yield [out, status] if block_given?
+
+        out
+      end
+
+      def available?(cmd)
+        command(['command', '-v', cmd, '>/dev/null']) do |_, status|
+          break status.success?
+        end
+      end
+
+      def check_container_running
+        containers = command(%w[docker ps --format {{.Names}}]).each_line(chomp: true)
+
+        running = Container.constants.map do |x|
+          name = Container.const_get(x)
+          containers.find { |c| c == name }&.tap { |c| puts "The docker container '#{c}' is already running." }
+        end
+
+        abort if running.any?
+      end
+
+      def init_superset
+        env = Dotenv.parse(Ldflow.home.join('.env'))
+
+        admin_password = env['SS_PASSWORD']
+
+        raise Error, 'SS_PASSWORD is not set' unless admin_password && !admin_password.empty?
+
+        cmd = compose + [
+          '-f', Ldflow.home.join('docker-compose.yml').to_s,
+          'run',
+          '--rm',
+          '--user', 'root',
+          '--env', %("ADMIN_PASSWORD=#{admin_password}"),
+          'superset',
+          '/app/docker/docker-init.sh'
+        ]
+
+        puts cmd.join(' ')
+
+        system(cmd, exception: true)
+      end
+    end
+  end
+end
